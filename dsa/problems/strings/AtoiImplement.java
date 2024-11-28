d957eeb9be989fd51df73ab37c799ca35f29886a
Nithish Kumar <nithishlearning45@gmail.com>
2024-11-28 16:44:34 +0530

    ATOI Implement

--- dsa/problems/strings/AtoiImplement.java
+++ dsa/problems/strings/AtoiImplement.java
@@ -5,30 +5,35 @@ package dsa.problems.strings;
 
 class AtoiImplement {
     public int myAtoi(String s) {
-        // Your code here
-        int i = 0;
+
+        int i =0;
         int n = s.length();
-        int res = 0;
-        while (i < n && s.charAt(i) == ' ') {
+        while(i<n && s.charAt(i)==' '){
             i++;
         }
 
-        int sign = 1;
-        while (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
-            sign = (s.charAt(i) == '-') ? -1 : 1;
+        int sign =1;
+        if(i<n && (s.charAt(i)=='+' || s.charAt(i)=='-')){
+            sign = (s.charAt(i)=='-') ? -1:1;
             i++;
         }
+        // Ensure no multiple signs
+        if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
+            return 0; // Invalid format
+        }
+        int res=0;
 
-        while (i < n && s.charAt(i) >= '0' && s.charAt(i) <= '9') {
-            int digit = s.charAt(i) - '0';
+        while(i<n && s.charAt(i)>='0' && s.charAt(i)<='9'){
+            int digi = s.charAt(i)-'0';
 
-            if (res > (Integer.MAX_VALUE - digit) / 10) {
-                return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            if(res>(Integer.MAX_VALUE -digi)/10){
+                return (sign==1) ?Integer.MAX_VALUE:Integer.MIN_VALUE;
             }
-            res = res * 10 + digit;
+            res= res*10+digi;
             i++;
         }
-        return res * sign;
+
+        return sign*res;
 
 
     }

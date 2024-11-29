b449e71b6f13b6e95a8cdfe54cc35bab34310ca4
Nithish Kumar <nithishlearning45@gmail.com>
2024-11-29 23:07:32 +0530

    Add binary

---
+++ dsa/problems/strings/AddBinary.java
@@ -0,0 +1,27 @@
+package dsa.problems.strings;
+
+class AddBinary {
+    public String addBinary(String a, String b) {
+        int i = a.length()-1;
+        int j = b.length()-1;
+        int carry =0;
+        StringBuilder sb = new StringBuilder();
+        while(i>=0 || j>=0 || carry ==1){
+            int curr = carry;
+            curr += i>=0 ? a.charAt(i--)-'0':0;
+            curr += j>=0 ? b.charAt(j--)-'0':0;
+            sb.append(curr%2);
+            carry = curr/2;
+
+        }
+
+        sb.reverse();
+        // int k =0;
+        // while(k<sb.length() && sb.charAt(k)=='0'){
+        //     k++;
+        // }
+
+        // sb.delete(0,k);
+        return sb.toString();
+    }
+}

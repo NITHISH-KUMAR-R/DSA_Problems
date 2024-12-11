88e7e2c962a0ad9665fbd9791da7a02bb4707064
Nithish Kumar <nithishlearning45@gmail.com>
2024-12-11 20:53:54 +0530

    merge array without extra space

---
+++ dsa/problems/arrays/MergeArray.java
@@ -0,0 +1,32 @@
+package dsa.problems.arrays;
+
+import java.util.Arrays;
+
+public @interface MergeArray {
+    class Solution {
+        // Function to merge the arrays.
+        public void mergeArrays(int a[], int b[]) {
+            // code here
+
+            int n = a.length;
+            int m  =b.length;
+
+            int i =n-1; int j =0;
+
+            while(i>=0 && j<m){
+                if(a[i]>b[j]){
+                    int temp = a[i];
+                    a[i] = b[j];
+                    b[j]=temp;
+
+                }
+
+                i--;
+                j++;
+            }
+
+            Arrays.sort(a);
+            Arrays.sort(b);
+        }
+    }
+}

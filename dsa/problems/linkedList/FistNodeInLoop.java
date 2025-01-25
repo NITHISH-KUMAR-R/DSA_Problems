623172befc56954bf1c41772dd314d7a83c60047
Nithish Kumar <nithishlearning45@gmail.com>
2025-01-25 13:25:36 +0530

    Happy Number

--- dsa/problems/linkedList/FistNodeInLoop.java
+++ dsa/problems/linkedList/FistNodeInLoop.java
@@ -2,6 +2,7 @@ package dsa.problems.linkedList;
 
 public class FistNodeInLoop {
     class Solution {
+        //Leetcode Happy number also same logic
         public static Node findFirstNode(Node head) {
             // code here
 
@@ -26,4 +27,27 @@ public class FistNodeInLoop {
         }
     }
 
+}
+
+class Solution {
+    //static int sum =0
+    public static int getSquare(int n){
+        int sum =0;
+        while(n>0){
+            int lDigit = n%10;
+            sum+=lDigit*lDigit;
+            n=n/10;
+        }
+        return  sum;
+    }
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast =n;
+        do{
+            slow = getSquare(slow);
+            fast = getSquare(getSquare(fast));
+        }while(slow!=fast);
+
+        return slow ==1;
+    }
 }

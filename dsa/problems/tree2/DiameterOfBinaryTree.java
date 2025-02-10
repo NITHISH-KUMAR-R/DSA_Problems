8b3a920ea6e96eefbafc50681b9db0594ff2d47f
Nithish Kumar <nithishlearning45@gmail.com>
2025-02-10 21:52:09 +0530

    Diameter GFG Code

--- dsa/problems/tree2/DiameterOfBinaryTree.java
+++ dsa/problems/tree2/DiameterOfBinaryTree.java
@@ -12,6 +12,7 @@ package dsa.problems.tree2;
 //Right Height: rh = height(root.right, dia)
 //Possible Diameter at this Node: lh + rh
 public class DiameterOfBinaryTree {
+
     public int height(Node root, int[] dia) {
         if (root == null) {
             return 0; // Base case: height of null node is 0
@@ -25,6 +26,24 @@ public class DiameterOfBinaryTree {
 
         // Return height of this subtree
         return 1 + Math.max(lh, rh);
+    }
+
+    //
+    class Solution {
+
+        public int height(Node root, int[]dia){
+            if(root==null){return 0;}
+            int lh = height(root.left,dia);
+            int rh = height(root.right,dia);
+            dia[0]= Math.max(dia[0],lh+rh);
+            return 1+ Math.max(lh,rh);
+        }
+        int diameter(Node root) {
+            // Your code here
+            int dia[] = new int[1];
+            height(root,dia);
+            return dia[0];
+        }
     }
 
 }

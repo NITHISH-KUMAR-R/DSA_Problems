b9bfea1173fd3a2d7a70bf36374032841438ff5d
Nithish Kumar <nithishlearning45@gmail.com>
2025-02-14 21:26:25 +0530

    Swap two Nodes in BST wrong position values

---
+++ dsa/problems/treeBst/SwapTwoNodesBST.java
@@ -0,0 +1,31 @@
+package dsa.problems.treeBst;
+
+public class SwapTwoNodesBST {
+    class Solution {
+        Node first =null , second =null, prev= null;
+        void check(Node root){
+            if(root==null){
+                return;
+            }
+            check(root.left);
+            if(prev!=null && prev.data>root.data){
+                if(first==null){
+                    first = prev;
+                }
+                second = root;
+            }
+            prev = root;
+            check(root.right);
+        }
+        void correctBST(Node root) {
+            // code here.
+            check(root);
+
+            if (first != null && second != null) {
+                int temp = first.data;
+                first.data = second.data;
+                second.data = temp;
+            }
+        }
+    }c
+}

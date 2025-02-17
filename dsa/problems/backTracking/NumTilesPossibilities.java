ff3d0312f6746dfe33467542f35969af6427d505
Nithish Kumar <nithishlearning45@gmail.com>
2025-02-17 07:34:04 +0530

    NumTiles possibilities leetcode

---
+++ dsa/problems/backTracking/NumTilesPossibilities.java
@@ -0,0 +1,25 @@
+package dsa.problems.backTracking;
+
+public class NumTilesPossibilities {
+    class Solution {
+
+        public int wayCount(int freq[]){
+            int count  =0;
+            for(int i =0;i<26;i++){
+                if(freq[i]>0){
+                    freq[i]--;
+                    count += 1+ wayCount(freq);
+                    freq[i]++;
+                }
+            }
+            return count;
+        }
+        public int numTilePossibilities(String tiles) {
+            int [] arr = new int[26];
+            for(char c : tiles.toCharArray()){
+                arr[c-'A']++;
+            }
+            return wayCount(arr);
+        }
+    }c
+}

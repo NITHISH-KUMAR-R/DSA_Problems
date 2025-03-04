c1dddf81d80f6914a439f834e27b51a080006eba
Nithish Kumar <nithishlearning45@gmail.com>
2025-03-04 15:02:07 +0530

    Longest Increasing SubSequence

---
+++ dsa/problems/doublyLinkedList/dp/LongestIncreasingSuqDeq.java
@@ -0,0 +1,29 @@
+package dsa.problems.doublyLinkedList.dp;
+
+import java.util.Arrays;
+
+public class LongestIncreasingSuqDeq {
+    class Solution {
+        static int lis(int arr[]) {
+            // code here
+            int n = arr.length;
+            if (n == 0) return 0;
+
+            int[] dp = new int[n];
+            Arrays.fill(dp, 1);
+            int maxLIS = 1;
+
+            for (int i = 1; i < n; i++) {
+                for (int j = 0; j < i; j++) {
+                    if (arr[i] > arr[j]) {
+                        dp[i] = Math.max(dp[i], dp[j] + 1);
+                    }
+                }
+                maxLIS = Math.max(maxLIS, dp[i]);
+            }
+
+            return maxLIS;
+
+        }
+    }
+}

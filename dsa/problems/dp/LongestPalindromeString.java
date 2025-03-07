8a7712ddb02d222dc1e72154ffb5605ebca9d647
Nithish Kumar <nithishlearning45@gmail.com>
2025-03-08 00:43:13 +0530

    longest Palindrome in a String

--- dsa/problems/dp/LongestPalindromeString.java
+++ dsa/problems/dp/LongestPalindromeString.java
@@ -2,36 +2,36 @@ package dsa.problems.dp;
 
 public class LongestPalindromeString {
     class Solution {
-        public static String longestPalindrome(String s) {
-            if (s == null || s.length() == 0) return "";
+        static String check(String s , int left, int right){
 
-            int maxLen = 0;
-            String longestPalindrome = "";
-
-            for (int i = 0; i < s.length(); i++) {
-                // Expand for odd-length palindrome
-                String oddPalindrome = expandAroundCenter(s, i, i);
-                if (oddPalindrome.length() > maxLen) {
-                    maxLen = oddPalindrome.length();
-                    longestPalindrome = oddPalindrome;
-                }
-
-                // Expand for even-length palindrome
-                String evenPalindrome = expandAroundCenter(s, i, i + 1);
-                if (evenPalindrome.length() > maxLen) {
-                    maxLen = evenPalindrome.length();
-                    longestPalindrome = evenPalindrome;
-                }
-            }
-            return longestPalindrome;
-        }
-
-        private static String expandAroundCenter(String s, int left, int right) {
-            while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            while(left >=0 && right<s.length() && s.charAt(left) == s.charAt(right)){
                 left--;
                 right++;
             }
-            return s.substring(left + 1, right);
+
+            return s.substring(left+1,right);
+        }
+        public static String longestPalindrome(String s) {
+            int maxLength =0;
+            String longest = "";
+
+            for(int i =0;i<s.length();i++){
+
+                String odd = check(s,i,i);
+                if(odd.length()>maxLength){
+                    maxLength = odd.length();
+                    longest = odd;
+                }
+
+                String even = check(s,i,i+1);
+                if(even.length()>maxLength){
+                    maxLength = even.length();
+                    longest = even;
+                }
+            }
+
+            return longest;
+
         }
     }
 

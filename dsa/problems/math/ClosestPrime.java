b422b8905969cc03136bbbc31a1a9409d37a04a8
Nithish Kumar <nithishlearning45@gmail.com>
2025-03-07 23:59:15 +0530

    Closest Prime

--- dsa/problems/math/ClosestPrime.java
+++ dsa/problems/math/ClosestPrime.java
@@ -2,11 +2,10 @@ package dsa.problems.math;
 
 public class ClosestPrime {
     class Solution {
-
         public int[] closestPrimes(int left, int right) {
             int prevPrime = -1, closestA = -1, closestB = -1;
-            int minDifference = (int) 1e6;
-            // Find all prime numbers in the given range
+            int minDifference = Integer.MAX_VALUE;
+
             for (int candidate = left; candidate <= right; candidate++) {
                 if (isPrime(candidate)) {
                     if (prevPrime != -1) {
@@ -16,29 +15,23 @@ public class ClosestPrime {
                             closestA = prevPrime;
                             closestB = candidate;
                         }
-                        // Twin prime optimization
-                        if (difference == 2 || difference == 1) return new int[] {
-                                prevPrime,
-                                candidate,
-                        };
                     }
                     prevPrime = candidate;
                 }
             }
 
-            return (closestA == -1)
-                    ? new int[] { -1, -1 }
-                    : new int[] { closestA, closestB };
+            return (closestA == -1) ? new int[] { -1, -1 } : new int[] { closestA, closestB };
         }
 
         private boolean isPrime(int number) {
-            if (number < 2) return false;
-            if (number == 2 || number == 3) return true;
-            if (number % 2 == 0) return false;
-            for (int divisor = 3; divisor * divisor <= number; divisor += 2) {
-                if (number % divisor == 0) return false;
+            if(number<2) return false;
+            if(number ==2 || number == 3) return true;
+            if(number%2==0) return false;
+            for(int i =3 ; i * i <= number ; i+=2){
+                if(number%i==0) return false;
             }
             return true;
         }
     }
+
 }

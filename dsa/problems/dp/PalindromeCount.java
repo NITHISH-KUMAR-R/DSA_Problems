0a9c5749240812851278cdeaf10ab57d4df85db7
Nithish Kumar <nithishlearning45@gmail.com>
2025-03-09 22:58:03 +0530

    Palindrome SubStrings

--- dsa/problems/dp/PalindromeCount.java
+++ dsa/problems/dp/PalindromeCount.java
@@ -31,3 +31,27 @@ public class PalindromeCount {
 
 //Expand around center
 //Palindrome SubStrings gfg
+
+//Leetcode 647. Palindromic Substrings
+
+class Solution {
+    public static int check(String s, int left, int right){
+        int count =0;
+        while(left>=0 && right <s.length() && s.charAt(left)== s.charAt(right)){
+            count++;
+            left--;
+            right++;
+        }
+        return count;
+    }
+
+    public int countSubstrings(String s) {
+        int count =0;
+        for(int i =0;i<s.length();i++){
+            count += check(s,i,i);
+            count += check(s,i,i+1);
+        }
+        return count;
+
+    }
+}

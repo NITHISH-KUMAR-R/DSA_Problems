30e60c3090edf4599477c7cfb3791926539a5143
Nithish Kumar <nithishlearning45@gmail.com>
2025-03-13 22:32:02 +0530

    0 - 1 Knapsack Problem

--- dsa/problems/dp/KnapSackDp.java
+++ dsa/problems/dp/KnapSackDp.java
@@ -8,8 +8,8 @@ public class KnapSackDp {
             int dp[] = new int[W + 1];
 
             for (int i = 0; i < n; i++) {
-                for (int w = W; w >= wt[i]; w--) {
-                    dp[w] = Math.max(dp[w], val[i] + dp[w - wt[i]]);
+                for (int j = W; j >= wt[i]; j--) {
+                    dp[j] = Math.max(dp[j], val[i] + dp[j - wt[i]]);
                 }
             }
             return dp[W];

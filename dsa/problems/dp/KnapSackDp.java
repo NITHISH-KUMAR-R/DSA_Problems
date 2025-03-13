121ea60a7b11ebd43f4adc345eaf910a721b4ad2
Nithish Kumar <nithishlearning45@gmail.com>
2025-03-13 22:35:05 +0530

    0 - 1 Knapsack Problem

--- dsa/problems/dp/KnapSackDp.java
+++ dsa/problems/dp/KnapSackDp.java
@@ -16,30 +16,31 @@ public class KnapSackDp {
 
         }
     }
-
-}
-
-
-
-class Solution2 {
-    // Function to return max value that can be put in knapsack of capacity W.
-    static int knapSack(int W, int wt[], int val[]) {
-        // your code here
-        int K[][] = new int[wt.length+1][W+1];
-        for(int i=0; i <= val.length; i++){
-            for(int j=0; j <= W; j++){
-                if(i == 0 || j == 0){
-                    K[i][j] = 0;
-                    continue;
-                }
-                if(j - wt[i-1] >= 0){
-                    K[i][j] = Math.max(K[i-1][j], K[i-1][j-wt[i-1]] + val[i-1]);
-                }else{
-                    K[i][j] = K[i-1][j];
+    class Solution2 {
+        // Function to return max value that can be put in knapsack of capacity W.
+        static int knapSack(int W, int wt[], int val[]) {
+            // your code here
+            int K[][] = new int[wt.length + 1][W + 1];
+            for (int i = 0; i <= val.length; i++) {
+                for (int j = 0; j <= W; j++) {
+                    if (i == 0 || j == 0) {
+                        K[i][j] = 0;
+                        continue;
+                    }
+                    if (j - wt[i - 1] >= 0) {
+                        K[i][j] = Math.max(K[i - 1][j], K[i - 1][j - wt[i - 1]] + val[i - 1]);
+                    } else {
+                        K[i][j] = K[i - 1][j];
+                    }
                 }
             }
+            return K[val.length][W];
         }
-        return K[val.length][W];
     }
+
 }
+
+
+
+
 //gfg0 - 1 Knapsack Problemc

74a6a7bcfb48eb6d00c41c74f561f177cd5f954d
Nithish Kumar <nithishlearning45@gmail.com>
2025-03-14 23:43:40 +0530

    CoinChnageWays

---
+++ dsa/problems/dp/CoinChangeWays.java
@@ -0,0 +1,22 @@
+package dsa.problems.dp;
+
+public class CoinChangeWays {
+    class Solution {
+        public int count(int coins[], int sum) {
+            int[] dp = new int[sum + 1];
+            dp[0] = 1; // Base case: 1 way to make sum 0 (by choosing nothing)
+
+            for (int coin : coins) {
+                for (int j = coin; j <= sum; j++) {
+                    dp[j] += dp[j - coin];
+                }
+            }
+
+            return dp[sum];
+        }
+    }
+}
+
+///Input: coins[] = [1, 2, 3], sum = 4
+//Output: 4
+//Explanation: Four Possible ways are: [1, 1, 1, 1], [1, 1, 2], [2, 2], [1, 3].

cb357a3bc35583e4230871b4fc671db5c4666b1c
Nithish Kumar <nithishlearning45@gmail.com>
2025-03-16 22:56:52 +0530

    Minimum Jumps Greedy

--- dsa/problems/greedy/MinimumJumps.java
+++ dsa/problems/greedy/MinimumJumps.java
@@ -11,10 +11,14 @@ public class MinimumJumps {
             for(int i =0;i<arr.length;i++){
 
                 maxIndex = Math.max(maxIndex,arr[i]+i);
-                if(i==curr){
-                    jumps++;
-                    curr = maxIndex;
+                if (i == curr) {
+                    //we can ignore these two if block as well
+                    if (curr >= arr.length - 1) return jumps; // If we reached the last index, return the number of jumps taken.
+                    if (curr == maxIndex) return -1; // If we are stuck and can't move further, return -1.
+                    jumps++; // Increase jump count as we need a new jump to move forward.
+                    curr = maxIndex; // Update the current boundary to the farthest reachable index.
                 }
+
                 if (curr >= arr.length - 1) {
                     return jumps;
                 }

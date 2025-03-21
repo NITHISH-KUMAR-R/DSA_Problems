a091c2a20158658ab7d9530a4c604714261e4786
Nithish Kumar <nithishlearning45@gmail.com>
2025-03-21 23:37:38 +0530

    House Robber2

--- dsa/problems/dp/HouseRobber2.java
+++ dsa/problems/dp/HouseRobber2.java
@@ -20,4 +20,48 @@ public class HouseRobber2 {
             return Math.max(maxLoot(nums, 0, n - 2), maxLoot(nums, 1, n - 1));
         }
     }
+
+    //Used Same Approach
+    class Solution2 {
+        static int robHelper(int nums[]){
+            int n = nums.length;
+            if (n == 0) return 0;
+            if (n == 1) return nums[0];
+            if(nums.length<2){
+                return 0;
+            }
+
+            int dp [] = new int[nums.length];
+            dp[0] = nums[0];
+            dp[1] = Math.max(nums[0],nums[1]);
+
+            for(int i =2;i<nums.length;i++){
+                dp[i] = Math.max(dp[i-2]+nums[i] , dp[i-1]);
+            }
+
+            return dp[nums.length-1];
+        }
+
+
+        public int rob(int[] nums) {
+            int n = nums.length;
+            if (n == 0) return 0;
+            if (n == 1) return nums[0];
+            if(nums.length<2){
+                return 0;
+            }
+
+            int skipFirstHouse [] = new int[nums.length-1];
+            int skipLastHouse [] = new int[nums.length-1];
+
+            for (int i =0;i<nums.length-1;i++){
+                skipFirstHouse[i] = nums[i];
+                skipLastHouse[i] = nums[i+1];
+
+            }
+            return Math.max(robHelper(skipFirstHouse),robHelper(skipLastHouse));
+
+
+        }
+    }
 }

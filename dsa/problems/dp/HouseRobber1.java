245d4c0d2da75b0ed886d228e30abb8b3f84e561
Nithish Kumar <nithishlearning45@gmail.com>
2025-03-21 22:52:46 +0530

    House Robber1

--- dsa/problems/dp/HouseRobber1.java
+++ dsa/problems/dp/HouseRobber1.java
@@ -20,4 +20,23 @@ public class HouseRobber1 {
             return dp[arr.length-1];
         }
     }
+
+    class Solution2 {
+        public int findMaxSum(int arr[]) {
+            int n = arr.length;
+            if (n == 0) return 0;
+            if (n == 1) return arr[0];
+
+            int prev2 = 0, prev1 = arr[0];
+
+            for (int i = 1; i < n; i++) {
+                int curr = Math.max(prev2 + arr[i], prev1);
+                prev2 = prev1;
+                prev1 = curr;
+            }
+
+            return prev1;
+        }
+    }
+
 }

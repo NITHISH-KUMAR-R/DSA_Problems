f577f78a7250da4fb88bb6cdde553d38db78a208
Nithish Kumar <nithishlearning45@gmail.com>
2025-04-08 23:59:43 +0530

    Weighted graph

--- dsa/problems/graph/MainClassForWeightedGraph.java
+++ dsa/problems/graph/MainClassForWeightedGraph.java
@@ -4,7 +4,7 @@ public class MainClassForWeightedGraph {
     public static void main(String[] args) {
         WeightedGraph graph = new WeightedGraph(5);
         graph.addDirectedEdge(1, 2, 5);
-        graph.addDirectedEdge(2, 1, 5);
+        graph.addDirectedEdge(2, 1, 9);
         graph.addDirectedEdge(2, 4, 3);
         graph.addDirectedEdge(4, 1, 8);
         graph.addDirectedEdge(3, 2, 6);

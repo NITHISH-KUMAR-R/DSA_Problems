40f9be07d0164eac6ab555e037792d4c801f49bc
Nithish Kumar <nithishlearning45@gmail.com>
2025-04-08 23:59:29 +0530

    triangle leetcode

--- dsa/problems/graph/MainClassForWeightedGraph.java
+++ dsa/problems/graph/MainClassForWeightedGraph.java
@@ -5,6 +5,7 @@ public class MainClassForWeightedGraph {
         WeightedGraph graph = new WeightedGraph(5);
         graph.addDirectedEdge(1, 2, 5);
         graph.addDirectedEdge(2, 1, 5);
+        graph.addDirectedEdge(2, 4, 3);
         graph.addDirectedEdge(4, 1, 8);
         graph.addDirectedEdge(3, 2, 6);
         System.out.println(graph);

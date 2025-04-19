db54eab2f09dcde3f7846c520e682ee81fc84b1e
Nithish Kumar <nithishlearning45@gmail.com>
2025-04-19 23:55:30 +0530

    Maximal Square

---
+++ dsa/problems/DpPart2/matrix/MaximalSquare.java
@@ -0,0 +1,39 @@
+package dsa.problems.DpPart2.matrix;
+
+public class MaximalSquare {
+    class Solution {
+        public int maximalSquare(char[][] matrix) {
+            int m = matrix.length;
+            int n  = matrix[0].length;
+            int dp[][] = new int [m][n];
+            int result = 0;
+            // First row and column need special handling
+            for (int i = 0; i < m; i++) {
+                dp[i][0] = matrix[i][0] - '0';
+                result = Math.max(result, dp[i][0]);
+            }
+
+            for (int j = 0; j < n; j++) {
+                dp[0][j] = matrix[0][j] - '0';
+                result = Math.max(result, dp[0][j]);
+            }
+
+
+            // Fill rest of the DP table
+            for (int i = 1; i < m; i++) {
+                for (int j = 1; j < n; j++) {
+                    if (matrix[i][j] == '1') {
+                        dp[i][j] = 1 + Math.min(
+                                dp[i - 1][j - 1],
+                                Math.min(dp[i - 1][j], dp[i][j - 1])
+                        );
+                        result = Math.max(result, dp[i][j]);
+                    }
+                }
+            }
+
+            return result * result; // Return area
+
+        }
+    }
+}

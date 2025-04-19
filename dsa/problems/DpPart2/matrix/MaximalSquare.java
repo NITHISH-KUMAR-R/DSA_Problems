591ae81a999cef53d751dff20ef6427dfd7ff387
Nithish Kumar <nithishlearning45@gmail.com>
2025-04-20 00:04:38 +0530

    Maximal Square GFG

--- dsa/problems/DpPart2/matrix/MaximalSquare.java
+++ dsa/problems/DpPart2/matrix/MaximalSquare.java
@@ -36,4 +36,36 @@ public class MaximalSquare {
 
         }
     }
+
+    class Solution2GFG {
+        static int maxSquare(int mat[][]) {
+            // code here
+            int m = mat.length;
+            int n = mat[0].length;
+
+            int dp [][] = new int [m][n];
+            int result = 0;
+            for(int i =0;i<m;i++){
+                dp[i][0] = mat[i][0];
+                result = Math.max(result,dp[i][0]);
+            }
+            for(int i =0;i<n;i++){
+                dp[0][i] = mat[0][i];
+                result = Math.max(result,dp[0][i]);
+            }
+
+            for(int i =1;i<m;i++){
+                for(int j =1;j<n;j++){
+                    if(mat[i][j]==1){
+                        dp[i][j] =1+ Math.min(dp[i-1][j],Math.min(dp[i-1][j-1],dp[i][j-1]));
+                        result = Math.max(dp[i][j],result);
+
+                    }
+
+                }
+            }
+
+            return result;
+        }
+    }
 }

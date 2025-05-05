8a1011dee295db67ff06d02a75ca858e106e20ac
Nithish Kumar <nithishlearning45@gmail.com>
2025-05-05 23:51:33 +0530

    Search in almost sorted array

---
+++ dsa/problems/binarySearch/SearchinAlmostSortedArray.java
@@ -0,0 +1,32 @@
+package dsa.problems.binarySearch;
+
+public class SearchinAlmostSortedArray {
+    class Solution {
+        public int findTarget(int arr[], int target) {
+            int i = 0;
+            int j = arr.length - 1;
+
+            while (i <= j) {
+                int mid = i + (j - i) / 2;
+
+                // Check mid
+                if (arr[mid] == target) return mid;
+
+                // Check mid - 1
+                if (mid - 1 >= i && arr[mid - 1] == target) return mid - 1;
+
+                // Check mid + 1
+                if (mid + 1 <= j && arr[mid + 1] == target) return mid + 1;
+
+                // Move the search window
+                if (target < arr[mid]) {
+                    j = mid - 2;
+                } else {
+                    i = mid + 2;
+                }
+            }
+
+            return -1;
+        }
+    }
+}

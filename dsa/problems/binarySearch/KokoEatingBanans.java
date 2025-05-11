33427194848b7a5d8756e32f927d62bacaa95209
Nithish Kumar <nithishlearning45@gmail.com>
2025-05-11 21:29:25 +0530

    Add out/ directory to .gitignore and remove compiled files from tracking

--- dsa/problems/binarySearch/KokoEatingBanans.java
+++ dsa/problems/binarySearch/KokoEatingBanans.java
@@ -1,34 +1,43 @@
 package dsa.problems.binarySearch;
 
 public class KokoEatingBanans {
-    class Solution {
-        public static boolean canEatInTime(int [] piles,int givenSpeed, int givenHour){
-            int hour =0;
-            for(int pile:piles){
-                //hour += (pile+givenSpeed-1)/givenSpeed // Equivalent to ceiling(pile / givenSpeed)
-                hour += (int)Math.ceil((double)pile/givenSpeed);
-            }
-            return hour<=givenHour;
+    public static boolean canEatInTime(int[] piles, int givenSpeed, int givenHour) {
+        int hour = 0;
+        for (int pile : piles) {
+            //hour += (pile+givenSpeed-1)/givenSpeed // Equivalent to ceiling(pile / givenSpeed)
+            hour += (int) Math.ceil((double) pile / givenSpeed);
         }
-        public int minEatingSpeed(int[] piles, int h) {
-            int leftSpeed = 1;
-            int rightSpeed =0;
+        return hour <= givenHour;
+    }
 
-            for(int pile:piles){
-                rightSpeed = Math.max(pile,rightSpeed);
-            }
+    public static int minEatingSpeed(int[] piles, int h) {
+        int leftSpeed = 1;
+        int rightSpeed = 0;
 
-            while(leftSpeed<rightSpeed){
-                int mid = leftSpeed+ (rightSpeed-leftSpeed)/2;
-
-                if(canEatInTime(piles,mid,h)){
-                    rightSpeed = mid;
-                }else{
-                    leftSpeed = mid+1;
-                }
-            }
-            return leftSpeed;
-
+        for (int pile : piles) {
+            rightSpeed = Math.max(pile, rightSpeed);
         }
+
+        while (leftSpeed < rightSpeed) {
+            int mid = leftSpeed + (rightSpeed - leftSpeed) / 2;
+
+            if (canEatInTime(piles, mid, h)) {
+                rightSpeed = mid;
+            } else {
+                leftSpeed = mid + 1;
+            }
+        }
+        return leftSpeed;
+
+    }
+
+
+    public static void main(String[] args) {
+        int []piles = {3,6,7,11}; int h = 8 ;
+        System.out.println("Coco can eat "+minEatingSpeed(piles, h));
+
+        //canEatInTime(piles,6,8);
+        //System.out.println(Math.ceil(double)10/6));
+
     }
 }

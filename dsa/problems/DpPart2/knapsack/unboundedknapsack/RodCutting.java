9a1e599909535eeb7eb1a8120cb259988f118e5e
Nithish Kumar <nithishlearning45@gmail.com>
2025-08-10 19:52:30 +0530

    Rod Cutting i and j  len and rod

---
+++ dsa/problems/DpPart2/knapsack/unboundedknapsack/RodCutting.java
@@ -0,0 +1,35 @@
+package dsa.problems.DpPart2.knapsack.unboundedknapsack;
+
+public class RodCutting {
+        public int cutRod(int[] price) {
+            // code here
+            int n  = price.length;
+
+            int dp[] = new int[n+1];
+
+            for(int len =1;len<=n;len++){
+                for(int rod = len ;rod<=n;rod++){
+                    dp[rod] = Math.max(dp[rod], dp[rod-len]+price[len-1]);
+                }
+            }
+
+            return dp[n];
+        }
+
+    class Solution {
+        public int cutRod(int[] price) {
+            // code here
+            int n  = price.length;
+
+            int dp[] = new int[n+1];
+
+            for(int i =1;i<=n;i++){
+                for(int j = i ;j<=n;j++){
+                    dp[j] = Math.max(dp[j], dp[j-i]+price[i-1]);
+                }
+            }
+
+            return dp[n];
+        }
+    }
+}

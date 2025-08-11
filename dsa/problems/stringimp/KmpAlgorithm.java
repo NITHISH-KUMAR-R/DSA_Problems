1b623a98e8fcc9dff55713e8665d07b7532a3759
Nithish Kumar <nithishlearning45@gmail.com>
2025-08-11 13:48:56 +0530

    KMP Algorithm

---
+++ dsa/problems/stringimp/KmpAlgorithm.java
@@ -0,0 +1,58 @@
+package dsa.problems.stringimp;
+
+import java.util.ArrayList;
+
+public class KmpAlgorithm {
+        public int[] lps(String p){
+            int i =1;
+            int j =0;
+            int dp[] = new int[p.length()];
+
+            while(i<p.length()){
+                if(p.charAt(i)==p.charAt(j)){
+
+                    dp[i] = j+1;
+                    i++;
+                    j++;
+                }else{
+                    if(j!=0){
+                        j= dp[j-1];
+                    }else{
+                        i++;
+                    }
+                }
+            }
+            return dp;
+        }
+        ArrayList<Integer> search(String pat, String txt) {
+            // code here
+            int dp[] = lps(pat);
+            int i =0;
+            int j =0;
+            ArrayList<Integer> list =  new ArrayList<>();
+            while(i<txt.length()){
+                if(txt.charAt(i)==pat.charAt(j)){
+                    i++;
+                    j++;
+
+                    if(j==pat.length()){
+                        list.add(i-j);
+                        j = dp[j-1];
+                    }
+                }else{
+                    if(j!=0){
+                        j= dp[j-1];
+                    }else{
+                        i++;
+                    }
+                }
+            }
+
+            return list;
+            //The LPS array tells us how much we can shift the pattern
+            //when a mismatch happens, without re-checking characters we already know match.
+
+
+        }
+    }
+
